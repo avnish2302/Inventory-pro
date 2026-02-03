1374bf182b1ff4d81b641354ea75421bd58687d7
avnish2302 <avnish2302@gmail.com>
2026-02-04 00:26:22 +0530

    update cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,9 @@ import salesRoutes from "./routes/sales.routes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
